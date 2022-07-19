205a57f6e15329627345f2fe48ebb622d4f92c08
Manthan Bhatt <manthan@markettime.com>
2022-07-19 11:57:35 +0530

    Fix for the max sub array solution

--- 3-maximum-subarray.js
+++ 3-maximum-subarray.js
@@ -15,7 +15,7 @@
 
 const maxSubArray = (nums) => {
     if (nums.length == 0) return 0;
-    let result = 0;
+    let result = -Infinity;
     let sum = 0;
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
